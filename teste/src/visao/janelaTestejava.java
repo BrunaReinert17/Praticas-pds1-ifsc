e617463b799f87acc331767b7d76c26fde2a71fe
Bruna R <carolinereinertbruna@gmail.com>
2023-02-07 08:55:42 -0300

    praticas aula

---
+++ teste/src/visao/janelaTestejava.java
@@ -0,0 +1,21 @@
+package visao;
+
+import javax.swing.JOptionPane;
+
+public class janelaTestejava {
+
+	public static void main(String[] args) {
+				
+//		String digitado = JOptionPane.showInputDialog("Digite alguma coisa:");
+		
+//		if(!digitado.isEmpty()) {
+//			JOptionPane.showMessageDialog(null, digitado);
+//		}
+		
+//		JOptionPane.showConfirmDialog(null, "Quer café?");
+//	}
+	
+	int opcao = JOptionPane.showConfirmDialog(null, "mensagem");
+}
+}
+

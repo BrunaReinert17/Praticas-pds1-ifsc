7cfa356d38ea7a55d4aa4a4cfdc8889d3a617aa1
Bruna R <carolinereinertbruna@gmail.com>
2023-02-14 09:05:11 -0300

    Interface Grafica
    Atuaizando o codigo

--- teste/src/controle/FuncionarioDAO.java
+++ teste/src/controle/FuncionarioDAO.java
@@ -1,5 +1,19 @@
+
 package controle;
+
+import modelo.Funcionario;
 
 public class FuncionarioDAO {
 
+	public void setNome(String nome) {
+		
+		
+	}
+
+	public static FuncionarioDAO getInstance() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 }
--- teste/src/visao/JanelaCadastroPessoa.java
+++ teste/src/visao/JanelaCadastroPessoa.java
@@ -6,9 +6,23 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import controle.FuncionarioDAO;
+import modelo.Funcionario;
+
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class JanelaCadastroPessoa extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtNome;
+	private JTextField txtCpf;
 
 	/**
 	 * Launch the application.
@@ -30,12 +44,61 @@ public class JanelaCadastroPessoa extends JFrame {
 	 * Create the frame.
 	 */
 	public JanelaCadastroPessoa() {
+		setBackground(new Color(255, 255, 255));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 374, 230);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 192, 203));
+		contentPane.setForeground(new Color(192, 192, 192));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtNome = new JTextField();
+		txtNome.setBounds(97, 26, 156, 20);
+		contentPane.add(txtNome);
+		txtNome.setColumns(10);
+		
+		txtCpf = new JTextField();
+		txtCpf.setBounds(97, 55, 156, 20);
+		contentPane.add(txtCpf);
+		txtCpf.setColumns(10);
+		
+		JLabel Nome = new JLabel("Nome:");
+		Nome.setBounds(47, 26, 83, 20);
+		contentPane.add(Nome);
+		
+		JLabel Cpf = new JLabel("CPF:");
+		Cpf.setBounds(47, 57, 83, 17);
+		contentPane.add(Cpf);
+		
+		JButton bntSalvar = new JButton("Salvar");
+		bntSalvar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				//Pega o texto do cargo de texto e armazena
+				//nas vairaveis para manipula-las
+				String nome = txtNome.getText();
+				String cpf  = txtCpf.getText();
+				
+				if(nome.isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Nenhum nome preenchido!");
+				}
+				if(cpf.isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Nenhum cpf preenchido!");
+				}
+				
+				Funcionario func = new Funcionario();
+				func.setNome(nome);
+				
+				FuncionarioDAO bdPessoa = FuncionarioDAO.getInstance();
+				bdPessoa.inserir(func);
+				
+			}
+			
+		});
+		bntSalvar.setBounds(118, 86, 108, 31);
+		contentPane.add(bntSalvar);
 	}
-
 }

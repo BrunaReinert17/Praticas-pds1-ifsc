1db6cc7312240735903345844fc0ec39c5801b52
Bruna Reinert <carolinereinertbruna@gmail.com>
2023-02-28 09:08:16 -0300

    Lista 1
    Questão 1 e 2 incompletas

---
+++ Lista1/src/Questoeslista/Exercicio1.java
@@ -0,0 +1,17 @@
+package Questoeslista;
+
+import javax.swing.JOptionPane;
+//Faça um programa que lê precoLitro (tipo Double) e quantidadeLitros (tipo Integer)
+//usando o método showInputDialog da classe JOptionPane. Crie um método (função) que
+//calcule o totalPagamento e use ele para calcular os valores inseridos. Exiba o valor final
+//utilizando o método showMessageDialog da classe JOptionPane.
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+	
+		Double precoLitro = Double.valueOf(JOptionPane.showInputDialog("Digite o preço do litro"));
+		
+	}
+		
+}

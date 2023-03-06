8c176c5387b35af836de0a3c34c1f51225283b1c
Bruna <carolinereinert@gmail.com>
2023-03-06 17:51:47 -0600

    Questao 1 e 3

--- Lista1/src/Questoeslista/Exercicio1.java
+++ Lista1/src/Questoeslista/Exercicio1.java
@@ -21,5 +21,3 @@ public class Exercicio1 {
 		return precoLitro * quantidadeLitros;
 	}
 }
-		
-
--- Lista1/src/Questoeslista/Exercicio3.java
+++ Lista1/src/Questoeslista/Exercicio3.java
@@ -7,12 +7,12 @@ public class Exercicio3 {
 public static void main(String[] args) {
         
         
-        double nota1 = Double.parseDouble(JOptionPane.showInputDialog("Digite a 1° nota:"));
-        double nota2 = Double.parseDouble(JOptionPane.showInputDialog("Digite a 2° nota:"));
-        double nota3 = Double.parseDouble(JOptionPane.showInputDialog("Digite a 3° nota:"));
+        double not1 = Double.parseDouble(JOptionPane.showInputDialog("Digite a 1° nota:"));
+        double not2 = Double.parseDouble(JOptionPane.showInputDialog("Digite a 2° nota:"));
+        double not3 = Double.parseDouble(JOptionPane.showInputDialog("Digite a 3° nota:"));
         
        
-        double media = (nota1 + nota2 + nota3) / 3;
+        double media = (not1 + not2 + not3) / 3;
         
        
         if (media >= 6) {

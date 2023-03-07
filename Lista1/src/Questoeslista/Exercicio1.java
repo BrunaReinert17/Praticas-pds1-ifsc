74f7ae045af898c3c70e650c6f298f54698ac3fe
Bruna <carolinereinert@gmail.com>
2023-03-06 18:08:51 -0600

    codigos certos

--- Lista1/src/Questoeslista/Exercicio1.java
+++ Lista1/src/Questoeslista/Exercicio1.java
@@ -6,14 +6,14 @@ public class Exercicio1 {
 
 	public static void main(String[] args) {
 
-		// Lê o preço do litro e a quantidade de litros
+		
 		double precoLitro = Double.parseDouble(JOptionPane.showInputDialog("Digite o preço do litro:"));
 		int quantidadeLitros = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade de litros:"));
 
-		// Calcula o total a ser pago
+		
 		double totalPagamento = calcularTotalPagamento(precoLitro, quantidadeLitros);
 
-		// Exibe o resultado
+		
 		JOptionPane.showMessageDialog(null, "Total a pagar: R$ " + totalPagamento);
 	}
 
